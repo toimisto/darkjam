0c8d9fa19380a76dae44d5c66e4608d0200999d6
jiisloth <joonas.val@gmail.com>
2019-12-14 16:26:16 +0200

    made levelgeneration infinite

--- scripts/LevelGenerator.gd
+++ scripts/LevelGenerator.gd
@@ -7,27 +7,37 @@ export(PackedScene) var Spike
 # var b = "text"
 var col = 0
 var row = 10
+var prev = []
 
-
-func add_block(prev, this_col, h):
+func add_block(this_col, h):
     match randi()%4:
         0:
-            var block = Block.instance()
-            add_child(block)
-            block.position.y = row*50-h*50
-            block.position.x = col*50
-            this_col.push_back("b")
-            add_block(prev, this_col, h+1)
+            var b = 1000
+            for i in range(5):
+                if prev[-i][-1] == "b":
+                    b = min(h - prev[-i].size(), b)
+            if b < 5:
+                var block = Block.instance()
+                add_child(block)
+                block.position.y = row*50-h*50
+                block.position.x = col*50
+                this_col.push_back("b")
+                add_block(this_col, h+1)
         1:
             if this_col[-1] == "b":
-                var spike = Spike.instance()
-                add_child(spike)
-                spike.position.y = row*50-h*50
-                spike.position.x = col*50
-                this_col.push_back("s")
+                var s = 0
+                for i in range(4):
+                    if prev[-i][-1] == "s":
+                        s += 1
+                if s != 4:
+                    var spike = Spike.instance()
+                    add_child(spike)
+                    spike.position.y = row*50-h*50
+                    spike.position.x = col*50
+                    this_col.push_back("s")
         2:
             this_col.push_back("")
-            add_block(prev, this_col, h+1)
+            add_block(this_col, h+1)
         3:
             pass
     if this_col[-1] == "":
@@ -35,7 +45,7 @@ func add_block(prev, this_col, h):
     return this_col
 
 
-func make_col(prev):
+func make_col():
     var this_col = ["b"]
     var block = Block.instance()
     add_child(block)
@@ -43,19 +53,16 @@ func make_col(prev):
     block.position.x = col*50
     
     if prev.size() > 10:
-        this_col = add_block(prev, this_col, 1)
+        this_col = add_block(this_col, 1)
         prev.pop_front()
 
     prev.push_back(this_col)
 
     col += 1
-    return prev
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-    var prev = []
-    for i in range(50):
-        prev = make_col(prev)
+#func _ready():
+
     
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
