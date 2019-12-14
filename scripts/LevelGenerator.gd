02a37ae14e0bb337b9860d10d6bb73da343be9cb
jiisloth <joonas.val@gmail.com>
2019-12-14 18:20:39 +0200

    levelgenstuf

--- scripts/LevelGenerator.gd
+++ scripts/LevelGenerator.gd
@@ -10,13 +10,13 @@ var row = 10
 var prev = []
 
 func add_block(this_col, h):
-    var next = randi()%6 - h
+    var next = randi()%6
     match next:
         0:
             if this_col[-1] == "b":
                 var s = 0
                 for i in range(4):
-                    if prev[-(i-1)][-1] == "s":
+                    if prev[-(i+1)][-1] == "s":
                         s += 1
                 if s != 4:
                     var spike = Spike.instance()
@@ -29,17 +29,16 @@ func add_block(this_col, h):
             add_block(this_col, h+1)
         2:
             if this_col.size() == 1:
-                if prev[-1].size() > 3:
+                if prev[-1].size() > 2 and prev[-1][-1] == "b":
                     row -= 1
                 else:
                     row += 1
         _:
             var b = 0
             for i in range(4):
-                if prev[-(i-1)][-1] == "b":
-                    b = max(prev[-(i-1)].size(), b)
-                    print(prev[-(i-1)].size())
-            if h < b + 2:
+                if prev[-(i+1)][-1] == "b":
+                    b = max(prev[-(i+1)].size(), b)
+            if h < b + 3:
                 var block = Block.instance()
                 add_child(block)
                 block.position.y = row*50-h*50
